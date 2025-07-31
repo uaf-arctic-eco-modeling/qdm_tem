c188cc1093d4e8fe0e5d734f4dd3327a036a2e34
Helene Genet <helenegenet@HeleneMBook2022.routerlogin.net>
2025-07-31 07:51:08 -0800

    Add check for negative values of precip, nirr and vapor_press. Set these values to zero and output updated climate file.

--- QDM_monthly/5_downscaling_7_masking.py
+++ QDM_monthly/5_downscaling_7_masking.py
@@ -165,3 +165,64 @@ mask_update['X'].attrs={'standard_name':'projection_x_coordinate','long_name':'x
 
 mask_update.to_netcdf(os.path.join(out_path,'input','run-mask-update.nc'))
 
+
+
+### CORRECT THE FEW NEGATIVES HERE AND THERE
+
+
+clmt = xr.open_dataset(os.path.join(out_path,'input','historic-climate.nc'))
+runmask = xr.open_dataset(os.path.join(out_path,'input','run-mask.nc'))
+# Set negative values to zero
+clmt['precip'] = clmt['precip'].where(clmt['precip'] >= 0, 0)
+clmt['vapor_press'] = clmt['vapor_press'].where(clmt['vapor_press'] >= 0, 0)
+clmt['nirr'] = clmt['nirr'].where(clmt['nirr'] >= 0, 0)
+# Replace values to missing outside of the mask
+clmt = xr.merge([clmt, runmask])
+clmt['tair'] = clmt['tair'].where(clmt['run'] == 1.0, -999.)
+clmt['precip'] = clmt['precip'].where(clmt['run'] == 1.0, -999.)
+clmt['nirr'] = clmt['nirr'].where(clmt['run'] == 1.0, -999.)
+clmt['vapor_press'] = clmt['vapor_press'].where(clmt['run'] == 1.0, -999.)
+clmt = clmt.drop_vars('run')
+# Format the output netcdf file
+clmt['lat'].attrs={'standard_name':'latitude','units':'degree_north','_FillValue': -999.0}
+clmt['lon'].attrs={'standard_name':'longitude','units':'degree_east','_FillValue': -999.0}
+clmt['tair'].attrs={'standard_name':'air_temperature','units':'celsius','grid_mapping':'albers_conical_equal_area','_FillValue': -999.0}
+clmt['precip'].attrs={'standard_name':'precipitation_amount','units':'mm month-1','grid_mapping':'albers_conical_equal_area','_FillValue': -999.0}
+clmt['nirr'].attrs={'standard_name':'downwelling_shortwave_flux_in_air','units':'W m-2','grid_mapping':'albers_conical_equal_area','_FillValue': -999.0}
+clmt['vapor_press'].attrs={'standard_name':'water_vapor_pressure','units':'hPa','grid_mapping':'albers_conical_equal_area','_FillValue': -999.0}
+clmt['time'].attrs={'units':'days since 1901-1-1 0:0:0','long_name':'time','calendar':'365_day'}
+clmt.time.encoding['units'] = 'days since 1901-01-01 00:00:00'
+clmt.time.encoding['calendar'] = '365_day'
+clmt.time.encoding['long_name'] = 'time'
+clmt.to_netcdf(os.path.join(out_path,'input','historic-climate_gf.nc'),unlimited_dims='time')
+
+
+for mod in modlist:
+    print(mod)
+    for sc in sclist:
+        print(sc)
+		clmt = xr.open_dataset(os.path.join(out_path,'input/projected-climate_' + sc + '_' + mod + '.nc'))
+		# Set negative values to zero
+		clmt['precip'] = clmt['precip'].where(clmt['precip'] >= 0, 0)
+		clmt['vapor_press'] = clmt['vapor_press'].where(clmt['vapor_press'] >= 0, 0)
+		clmt['nirr'] = clmt['nirr'].where(clmt['nirr'] >= 0, 0)
+		# Replace values to missing outside of the mask
+		clmt = xr.merge([clmt, runmask])
+		clmt['tair'] = clmt['tair'].where(clmt['run'] == 1.0, -999.)
+		clmt['precip'] = clmt['precip'].where(clmt['run'] == 1.0, -999.)
+		clmt['nirr'] = clmt['nirr'].where(clmt['run'] == 1.0, -999.)
+		clmt['vapor_press'] = clmt['vapor_press'].where(clmt['run'] == 1.0, -999.)
+		clmt = clmt.drop_vars('run')
+		# Format the output netcdf file
+		clmt['lat'].attrs={'standard_name':'latitude','units':'degree_north','_FillValue': -999.0}
+		clmt['lon'].attrs={'standard_name':'longitude','units':'degree_east','_FillValue': -999.0}
+		clmt['tair'].attrs={'standard_name':'air_temperature','units':'celsius','grid_mapping':'albers_conical_equal_area','_FillValue': -999.0}
+		clmt['precip'].attrs={'standard_name':'precipitation_amount','units':'mm month-1','grid_mapping':'albers_conical_equal_area','_FillValue': -999.0}
+		clmt['nirr'].attrs={'standard_name':'downwelling_shortwave_flux_in_air','units':'W m-2','grid_mapping':'albers_conical_equal_area','_FillValue': -999.0}
+		clmt['vapor_press'].attrs={'standard_name':'water_vapor_pressure','units':'hPa','grid_mapping':'albers_conical_equal_area','_FillValue': -999.0}
+		clmt['time'].attrs={'units':'days since 1901-1-1 0:0:0','long_name':'time','calendar':'365_day'}
+		clmt.time.encoding['units'] = 'days since 1901-01-01 00:00:00'
+		clmt.time.encoding['calendar'] = '365_day'
+		clmt.time.encoding['long_name'] = 'time'
+		clmt.to_netcdf(os.path.join(out_path,'input/projected-climate_' + sc + '_' + mod + '_gf.nc'),unlimited_dims='time')
+
